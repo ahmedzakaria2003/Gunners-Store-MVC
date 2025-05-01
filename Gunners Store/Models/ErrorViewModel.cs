2003455264787f9aed6270c9adc945772a2e82c0
ahmedzakaria2003 <you@example.com>
2025-05-01 17:19:52 +0300

    Add project files.

---
+++ Gunners Store/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gunners_Store.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
